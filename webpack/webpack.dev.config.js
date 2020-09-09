fdf7a98328849c2e590ead0a1f6c6f9b0cae789f
Sander Hidding <shidding@outlook.com>
2020-09-09 20:34:30 +0200

    ⚙️ setup

---
+++ webpack/webpack.dev.config.js
@@ -0,0 +1,20 @@
+/** ----------------------------------------
+    Webpack
+ ---------------------------------------- */
+
+const merge = require('webpack-merge');
+const common = require('./webpack.common.config.js');
+
+/** ----------------------------------------
+    Configuration
+ ---------------------------------------- */
+
+module.exports = merge(common, {
+    mode: 'development',
+    optimization: {
+        minimize: false
+    },
+    output: {
+        filename: 'que.js'
+    }
+});
