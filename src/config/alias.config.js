eac6b7b1a7520f81753943abefe686607813d65e
Sander Hidding <shidding@outlook.com>
2020-09-10 08:42:23 +0200

    ✨ set alias for pkg build

---
+++ src/config/alias.config.js
@@ -0,0 +1,83 @@
+/** ----------------------------------------
+     Create Alias
+ ---------------------------------------- */
+
+const alias = [
+
+    {
+        name: '_setConfig',
+        replace: '_c'
+    },
+
+    {
+        name: '_setQuery',
+        replace: '_q'
+    },
+
+    {
+        name: '_mask',
+        replace: '_m'
+    },
+
+    {
+        name: '_unmask',
+        replace: '_u'
+    },
+
+    {
+        name: '_validMask',
+        replace: '_vm'
+    },
+
+    {
+        name: '_convertDate',
+        replace: '_cd'
+    },
+
+    {
+        name: '_parseData',
+        replace: '_p'
+    },
+
+    {
+        name: '_parseDate',
+        replace: '_pd'
+    },
+
+    {
+        name: '_parseNumber',
+        replace: '_pn'
+    },
+
+    {
+        name: '_handleArray',
+        replace: '_hd'
+    },
+
+    {
+        name: '_handleData',
+        replace: '_h'
+    },
+
+    {
+        name: '_handleEscape',
+        replace: '_he'
+    },
+
+    {
+        name: '_createUri',
+        replace: '_cu'
+    },
+
+    {
+        name: '_updateUri',
+        replace: '_uu'
+    }
+
+];
+
+/** ----------------------------------------
+    Exports
+ ---------------------------------------- */
+
+module.exports = alias;
