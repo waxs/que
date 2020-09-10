c7604832add9cf865efc2e4b92f3ef3e1121a6bc
Sander Hidding <shidding@outlook.com>
2020-09-10 08:42:10 +0200

    🐛 fix issue on update return object

--- src/methods/retrieve/_refresh.js
+++ src/methods/retrieve/_refresh.js
@@ -3,12 +3,12 @@
  ---------------------------------------- */
 
 function refresh(obj) {
-    this._data = [];
+    this._data = {};
     const data = Object.entries(obj);
 
     data.forEach(item => {
-        const [name, content] = item;
-        this._data[name] = content;
+        const [name, value] = item;
+        this._data[name] = value;
     });
 
     const uri = this._createUri();
