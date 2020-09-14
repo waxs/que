9b3a37b3ed14bff906450e95a9851371f63586f2
Sander Hidding <shidding@outlook.com>
2020-09-14 08:42:30 +0200

    🐛 check for data set

--- src/methods/retrieve/_add.js
+++ src/methods/retrieve/_add.js
@@ -6,7 +6,7 @@ function add(obj) {
     const data = Object.entries(obj);
 
     const duplicate = (name, value) => {
-        const isArray = Array.isArray(value);
+        const isArray = Array.isArray(this._data[name]);
         const data = this.data[name];
         this._data[name] = !data ? [] : data;
         return !this.config.duplicate && (isArray && !this._data[name].includes(value));
