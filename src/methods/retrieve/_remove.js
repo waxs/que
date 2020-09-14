dd354b7151eed9915a977610c5d72f42b8fee128
Sander Hidding <shidding@outlook.com>
2020-09-14 08:43:25 +0200

    🎨 add string support for remove method

--- src/methods/retrieve/_remove.js
+++ src/methods/retrieve/_remove.js
@@ -1,22 +1,38 @@
 /** ----------------------------------------
+    Utilties
+ ---------------------------------------- */
+
+import isType from '@util/_isType';
+
+/** ----------------------------------------
     Remove
  ---------------------------------------- */
 
 function remove(obj) {
     const data = Object.entries(obj);
+    const argType = isType(obj);
 
-    data.forEach(item => {
-        const [name, value] = item;
-        const isArray = Array.isArray(value);
-        const array = isArray ? value : value.split();
+    if(argType === 'string') {
+        this._data[obj] && delete this._data[obj];
+    }
 
-        this.data[name] && isArray && array.forEach(value => {
-            const index = this.data[name].indexOf(value);
-            index > -1 && this.data[name].splice(index, 1);
+    if(argType === 'object') {
+        data.forEach(item => {
+            const [name, value] = item;
+            const isArray = Array.isArray(value);
+            const array = isArray ? value : value.split();
+
+            console.log(array);
+
+            this.data[name] && isArray && array.forEach(value => {
+                const index = this.data[name].indexOf(value);
+                index > -1 && this.data[name].splice(index, 1);
+            });
+
+            !isArray && delete this.data[name];
         });
+    }
 
-        !isArray && delete this.data[name];
-    });
 
     const uri = this._createUri();
     this._updateUri(uri);
