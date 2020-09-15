32a4707081af94c3d888bd71c638323341c9a136
Sander Hidding <shidding@outlook.com>
2020-09-15 08:15:09 +0200

    ♻️ refactor hasDuplicates helper

--- src/methods/helpers/index.js
+++ src/methods/helpers/index.js
@@ -2,6 +2,7 @@
     Utilities
  ---------------------------------------- */
 
+import _hasDuplicates from './_hasDuplicate';
 import _init from './_init';
 import _setConfig from './_config';
 import _setQuery from './_query';
@@ -11,6 +12,7 @@ import _setQuery from './_query';
  ---------------------------------------- */
 
 export default {
+    _hasDuplicates,
     _init,
     _setConfig,
     _setQuery
